067a162a6c3dac382e311e6914cf14b3c1c0384d
unknown <jadebrosas143@gmail.com>
2026-06-23 08:43:01 +0800

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
-  <router-view /> 
+  <router-view />
 </template>
+
+<script setup>
+// Keep this file completely clean! 
+// Vue Router will automatically inject Layout.vue OR Login.vue into the router-view depending on the URL.
+</script>
+
+<style>
+/* You can leave any global CSS you had in here */
+</style>
